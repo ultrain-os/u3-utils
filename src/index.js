6f839407c3a447f8fad27ac4c7e054fd08f5be0b
benyasin <rgwapkk520>
2018-11-18 17:41:17 +0800

    add ecc

--- src/index.js
+++ src/index.js
@@ -1,49 +1,52 @@
-import assertThrows from './assert-throws';
-import clone from './clone';
-import deepEqual from './deep-equal';
-import waitResolveable from './wait-resolveable';
-import waitUntil from './wait-until';
-import wait from './wait';
-import waitForever from './wait-forever';
-import runForever from './run-forever';
-import randomString from './random-string';
-import randomNumber from './random-number';
-import resolveValues from './resolve-values';
-import performanceNow from './performance-now';
-import isPromise from './is-promise';
-import promisify from './promisify';
+import assertThrows from "./assert-throws";
+import clone from "./clone";
+import deepEqual from "./deep-equal";
+import waitResolveable from "./wait-resolveable";
+import waitUntil from "./wait-until";
+import wait from "./wait";
+import waitForever from "./wait-forever";
+import runForever from "./run-forever";
+import randomString from "./random-string";
+import randomNumber from "./random-number";
+import resolveValues from "./resolve-values";
+import performanceNow from "./performance-now";
+import isPromise from "./is-promise";
+import promisify from "./promisify";
+import ecc from "./ecc";
 
 const U3Utils = {
-    assertThrows,
-    clone,
-    deepEqual,
-    waitResolveable,
-    waitUntil,
-    wait,
-    waitForever,
-    runForever,
-    randomString,
-    randomNumber,
-    resolveValues,
-    performanceNow,
-    isPromise,
-    promisify
+  assertThrows,
+  clone,
+  ecc,
+  deepEqual,
+  waitResolveable,
+  waitUntil,
+  wait,
+  waitForever,
+  runForever,
+  randomString,
+  randomNumber,
+  resolveValues,
+  performanceNow,
+  isPromise,
+  promisify
 };
 export default U3Utils;
 
 export {
-    assertThrows,
-    clone,
-    deepEqual,
-    waitResolveable,
-    waitUntil,
-    wait,
-    waitForever,
-    runForever,
-    randomString,
-    randomNumber,
-    resolveValues,
-    performanceNow,
-    isPromise,
-    promisify
+  assertThrows,
+  clone,
+  ecc,
+  deepEqual,
+  waitResolveable,
+  waitUntil,
+  wait,
+  waitForever,
+  runForever,
+  randomString,
+  randomNumber,
+  resolveValues,
+  performanceNow,
+  isPromise,
+  promisify
 };
